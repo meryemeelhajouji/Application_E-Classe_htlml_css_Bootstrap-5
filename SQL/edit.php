df381633e7600bfc815b968d3172080953f4d3f5
meryemeelhajouji <meryemelhajouji.99@gmail.com>
2022-02-06 17:57:20 +0100

    edite the students

--- SQL/edit.php
+++ SQL/edit.php
@@ -1,14 +1,4 @@
-<?php
-   
-    $index = $_GET['index'];
 
-    $json = file_get_contents('student.json');
-    $data = json_decode($json, true);
-
-    $row = $data[$index];
-  
-   
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -22,6 +12,26 @@
  </head>
     
 <body>
+<?php
+   
+   require_once 'connection.php';
+   $id = $_GET['id'];
+   $read_student = "SELECT * FROM students WHERE id = $id";
+   $res = mysqli_query($conn,$read_student);
+    while ($row = mysqli_fetch_assoc($res)){
+
+    $image = $row['image'];
+   $name = $row['name'];
+   $email = $row['email'];
+   $phone = $row['phone'];
+   $nb = $row['enroll_number'];
+   $datead = $row['date_admission'];
+    }
+
+   
+  
+  
+?>
   <main>
      <div class=" bg-info d-flex justify-content-center align-items-center ">
       <div class="bg-light col-lg-5 p-5 rounded-3  ">
@@ -35,23 +45,23 @@
          <form  action="" method = "POST">
           <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Image </label>
-            <input type="file" class="form-control" id="exampleInputEmail1"placeholder="Entre your image" name="image"  value="<?php echo $row['img']; ?>">
+            <input type="file" class="form-control" id="exampleInputEmail1"placeholder="Entre your image" name="image"  value="<?php echo $image; ?>">
           
             <label for="exampleInputPassword1" class="form-label">Name</label>
-            <input type="tex" class="form-control "id="exampleInputPassword1"  placeholder="Entre your name" name="name"  value="<?php echo $row['Name']; ?>"> 
+            <input type="tex" class="form-control "id="exampleInputPassword1"  placeholder="Entre your name" name="name"  value="<?php echo $name; ?>"> 
          
             <label for="exampleInputEmail1" class="form-label">Email </label>
-            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Entre your email" name="email"  value="<?php echo $row['Email']; ?>" >
+            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Entre your email" name="email"  value="<?php echo $email; ?>" >
           
             <label for="exampleInputEmail1" class="form-label">Phone </label>
-            <input type="text" class="form-control" id="exampleInputEmail1"  placeholder="Entre your phone" name="phone"   value="<?php echo $row['Phone']; ?>">
+            <input type="text" class="form-control" id="exampleInputEmail1"  placeholder="Entre your phone" name="phone"   value="<?php echo $phone; ?>">
           
             <label for="exampleInputEmail1" class="form-label">Enroll Number </label>
-            <input type="text" class="form-control" placeholder="Entre your Enroll" id="exampleInputPassword1"   name="nb"  value="<?php echo $row['Enroll Number']; ?>">
+            <input type="text" class="form-control" placeholder="Entre your Enroll" id="exampleInputPassword1"   name="nb"  value="<?php echo $nb; ?>">
        
          
             <label for="exampleInputEmail1" class="form-label">Date of admission </label>
-            <input type="date" class="form-control mb-4"  placeholder="Entre your date" id="exampleInputPassword1"  name="date"  value="<?php echo $row['Date of admission']; ?>">
+            <input type="date" class="form-control mb-4"  placeholder="Entre your date" id="exampleInputPassword1"  name="date"  value="<?php echo $datead; ?>">
          
           <button type="submit"class="btn  btn-info w-100 text-white text-uppercase  " name="update"> 
             update
@@ -59,22 +69,17 @@
         </form>
       </div>
       <?php
-      if(isset($_POST['update'])){
-        $student = array(
-          'img'   =>$_POST["image"] ,
-          'Name'  => $_POST["name"] ,
-          'Email' =>$_POST["email"] ,
-          'Phone' =>$_POST["phone"] ,
-          'Enroll Number' =>$_POST["nb"] ,
-          'Date of admission' => $_POST["date"],
-          'icon1'=>' <i class="fas fa-pen mx-4 "></i>',
-          'icon2'=>' <i class="fas fa-trash   mx-4 "></i>'
-      
-        );
-        $data[$index] = $student;
-        $json = json_encode($data, JSON_PRETTY_PRINT);
-        file_put_contents('student.json', $json);
+    if (isset($_POST['update'])){
 
+     $image = $_POST["image"] ;
+    $name = $_POST["name"] ;
+    $email =$_POST["email"] ;
+    $phone =$_POST["phone"] ;
+    $Enroll_Number = $_POST["nb"] ;
+    $Date_admission = $_POST["date"];
+
+    $edite_student ="UPDATE students SET image ='$image',name ='$name',email ='$email',phone ='$phone',enroll_number ='$Enroll_Number',date_admission ='$Date_admission' ";
+    mysqli_query($conn,$edite_student);
 
         header('location: students.php');
       }
